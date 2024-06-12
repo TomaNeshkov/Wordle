e6d3bd0b76cec893f5c0ad59015acc26e0bf510a
TomaNeshkov <126071897+TomaNeshkov@users.noreply.github.com>
2024-06-12 20:36:02 +0300

    wordle (fixed)
    yay

---
+++ Project-Wordle/Wordle/Wordle/Wordle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wordle
+{
+    internal static class Wordle
+    {
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new WordleForm());
+        }
+    }
+}
